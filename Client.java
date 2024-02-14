1e75474b25a55531efab15e9886f4c6626139de3
giulio <giulioascani3c@gmail.com>
2024-02-14 11:45:42 +0100

    aggiunto client.java

--- Client.java
+++ Client.java
@@ -1,1 +1,18 @@
-C
+import java.net.*;
+import java.io.*;
+
+public class Client {
+    public static void main (String[] args) throws IOException {
+     String hostName = "localhost"; // Indirizzo del server
+     int port = 12345; // Porta del server
+    try (Socket socket = new Socket (hostName, port);
+            BufferedReader in = new BufferedReader (
+            new InputStreamReader(socket.getInputStream()))){
+        String fromServer;
+        while ((fromServer = in.readLine()) |= null) {
+          System.out.println("Server:" + fromServer); 
+          break; // Uscine dal loop dopo aver nicevuto il messaggio
+       }
+      }
+     }
+    }
